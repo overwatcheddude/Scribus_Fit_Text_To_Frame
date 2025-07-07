fdfe4677be4f6c8503325ae14eae2dd3d0592944
overwatcheddude <overwatcheddude@gmail.com>
2025-07-07 18:29:06 +0400

    Formatting, replaced `getObjectGeometry` with `getSize`. Remove `0, scribus.ALL` from `setFontSize`.

--- fit_text_to_frame.py
+++ fit_text_to_frame.py
@@ -1,7 +1,7 @@
-
 import sys
+
 try:
-    import scribus  
+    import scribus
 except ImportError:
     print("This script only works from within Scribus.")
     sys.exit(1)
@@ -14,14 +14,14 @@ except NameError:
     sys.exit(1)
 
 
-baselineWidth = 100.0       # mm
-baselineFontSize = 47.6      # pt
+baselineWidth = 100.0  # mm
+baselineFontSize = 47.6  # pt
 ratio = baselineFontSize / baselineWidth
 
 
 count = scribus.selectionCount()
 if count == 0:
-    scribus.messageBox('Error', 'No frame selected')
+    scribus.messageBox("Error", "No frame selected")
     sys.exit(1)
 
 
@@ -30,26 +30,25 @@ for i in range(count):
     try:
         scribus.getTextLength(frame)
     except scribus.WrongFrameTypeError:
-        scribus.messageBox('Error', 'Only text frames are supported')
+        scribus.messageBox("Error", "Only text frames are supported")
         continue
 
     # get current frame width
-    x, y, frameWidth, frameHeight = scribus.getObjectGeometry(frame)
+    frameWidth, frameHeight = scribus.getSize(frame)
 
-    
     desiredSize = frameWidth * ratio
-    scribus.setFontSize(desiredSize, frame, 0, scribus.ALL)
+    scribus.setFontSize(desiredSize, frame)
 
     while scribus.getTextLines(frame) > 1 and desiredSize > 0:
         desiredSize -= 0.1
-        scribus.setFontSize(desiredSize, frame, 0, scribus.ALL)
+        scribus.setFontSize(desiredSize, frame)
     # grow
     while scribus.textOverflows(frame) == 0 and scribus.getTextLines(frame) == 1:
         desiredSize += 0.1
-        scribus.setFontSize(desiredSize, frame, 0, scribus.ALL)
-    
+        scribus.setFontSize(desiredSize, frame)
+
     desiredSize -= 0.1
-    scribus.setFontSize(desiredSize, frame, 0, scribus.ALL)
+    scribus.setFontSize(desiredSize, frame)
 
 # refresh view
 scribus.redrawAll()
